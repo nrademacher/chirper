d8fd726de476f58388b200ea31fe13e80f4aa258
Nikolay Rademacher <hrrdx33@gmail.com>
2021-06-04 02:44:33 +0200

    feat: set up basic routing

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users')
+const tweets = require('./routes/api/tweets')
 
 const db = process.env.MONGO_URI;
-console.log(db)
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use('/api/users', users)
+app.use('/api/tweets', tweets)
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello world'));
