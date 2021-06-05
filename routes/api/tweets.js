6166213e3a0cdd03b1d2ff3fb92ccdb61159656a
Nikolay Rademacher <hrrdx33@gmail.com>
2021-06-05 16:24:28 +0200

    feat: add tweet getting and posting

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,6 +1,55 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the tweets route" }));
+const Tweet = require('../../models/Tweet');
+const validateTweetInput = require('../../validation/tweets');
+
+// Get all tweets
+router.get('/', (req, res) => {
+  Tweet.find()
+    .sort({ date: -1 })
+    .then((tweets) => res.json(tweets))
+    .catch((err) => res.status(404).json({ notweetsfound: 'No tweets found' }));
+});
+
+// Get a user's tweets
+router.get('/user/:userId', (req, res) => {
+  Tweet.find({ user: req.params.userId })
+    .then((tweets) => res.json(tweets))
+    .catch((err) =>
+      res.status(404).json({ notweetsfound: 'No tweets found from that user' })
+    );
+});
+
+// Get an individual tweet
+router.get('/:id', (req, res) => {
+  Tweet.findById(req.params.id)
+    .then((tweet) => res.json(tweet))
+    .catch((err) =>
+      res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+    );
+});
+
+// Post tweets on jwt-protected route
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateTweetInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(erros);
+    }
+
+    const newTweet = new Tweet({
+      text: req.body.text,
+      user: req.user.id,
+    });
+
+    newTweet.save().then((tweet) => res.json(tweet));
+  }
+);
 
 module.exports = router;
