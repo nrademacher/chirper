3dabf6e5e756f4138798babd62be765dc199cbd8
Nikolay Rademacher <hrrdx33@gmail.com>
2021-06-07 01:02:00 +0200

    fix: axios routes in dev env to localhost

--- frontend/src/util/tweet_api_util.js
+++ frontend/src/util/tweet_api_util.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const getTweets = () => {
-  return axios.get(`${process.env.NODE_ENV === 'development' && 'http://localhost:5000'}/api/tweets`)
+  return axios.get(`${process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : ''}/api/tweets`)
 };
 
 export const getUserTweets = id => {
-  return axios.get(`${process.env.NODE_ENV === 'development' && 'http://localhost:5000'}/api/tweets/user/${id}`)
+  return axios.get(`${process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : ''}/api/tweets/user/${id}`)
 };
 
 export const writeTweet = data => {
-  return axios.post(`${process.env.NODE_ENV === 'development' && 'http://localhost:5000'}/api/tweets`, data)
+  return axios.post(`${process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : ''}/api/tweets`, data)
 }
