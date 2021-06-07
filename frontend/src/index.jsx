74e8c7e127e681e12a0bd76b24e5b1981dcf2de5
Nikolay Rademacher <hrrdx33@gmail.com>
2021-06-07 23:20:15 +0200

    feat: better component folder/file naming

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (decodedUser.exp < currentTime) {
       store.dispatch(logout());
-      window.location.href = '/login';
+      window.history.href = '/login';
     }
   } else {
     store = configureStore({});
