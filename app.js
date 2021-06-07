7a3ab9ea4c71d95f1538f14a66224a0021b59cad
Nikolay Rademacher <hrrdx33@gmail.com>
2021-06-07 23:23:16 +0200

    feat: better component folder/file naming

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/api/users', users)
 app.use('/api/tweets', tweets)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/dist'));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   })
 }
